bae54dfb508812edaefd44e5f426914da0030303
FOCUSNETCOM\subhani <shaik.m@centrahub.com>
2025-02-10 14:56:49 +0530

    first commite

---
+++ LeetCode/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ////3174. Clear Digits
+            //Console.WriteLine("Enter a string with numbers");
+            //string s = Console.ReadLine();
+            //int j = 0;
+            //int count = 0;
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    j = 0;
+            //    count = 1;
+            //    if (s[i]=='0'|| s[i] == '1'|| s[i] == '2' || s[i] == '3' || s[i] == '4' || s[i] == '5' || s[i] == '6' || s[i] == '7' || s[i] == '8' || s[i] == '9') //if (int.TryParse(s[i].ToString(), out j)) 
+            //    {
+            //        j = i;
+            //        if (j > 0)
+            //        {
+            //            j = i - 1;
+            //            count = 2;
+            //        }
+            //        s=s.Remove(j, count);
+            //        i =i-count;
+            //    }
+            //}
+        }
+    }
+}
